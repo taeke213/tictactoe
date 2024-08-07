7a4b3d523e0d747444f1f36fb3cb74cfe52a1b49
taeke213 <git@github.com:taeke213/steenpapierschaar.git>
2024-08-07 13:29:57 +0200

    final

--- script.js
+++ script.js
@@ -20,14 +20,17 @@ let winner = false
 let winningplayer
 let submitbut = document.querySelector("#sub")
 let fingamediv = document.querySelector("#gamefinish")
+let plr1div = document.getElementById("player1")
+let plr2div = document.getElementById("player2")
 let form = document.querySelector(".form")
 let newgamebut = document.getElementById("newgame")
+let showwin = document.getElementById("showwin")
 newgamebut.addEventListener(("click"), () => startGame())
 let name1 
 let name2
 let game
 let gameobj
-let o
+let o = "x"
 
 submitbut.addEventListener(("click"), () => startGame())
 
@@ -36,8 +39,11 @@ function startGame(){
         container.firstChild.remove()
     }
     winner = false
+    container.style.backgroundColor = "transparent"
     fingamediv.style.display = "none"
     form.style.display = "block"
+    plr1div.textContent = ""
+    plr2div.textContent = ""
     let name1 = document.getElementById("name1").value
     let name2 = document.getElementById("name2").value
     if(name1 != "" && name2 != ""){
@@ -45,6 +51,8 @@ function startGame(){
         game = makegame(name1, name2)
         gameobj = game[2]
         form.style.display = "none"
+        plr1div.textContent = `${name1}: x`
+        plr2div.textContent = `${name2}: o`
     }
     document.querySelector("#name1").value = ""
     document.getElementById("name2").value = ""
@@ -55,34 +63,43 @@ function createGrid(){
         block.id = i
         block.classList.add("block")
         block.addEventListener(("mousedown"), claimBlock)
-        block.addEventListener(("click"), changeUI)
         block.addEventListener(("mouseup"), alertWinner) 
         let container = document.getElementById("container")
-        container.appendChild(block)
+        container.appendChild(block)        
+        fingamediv.style.display = "block"
+        newgamebut.style.display = "none"
+        showwin.textContent = `player's turn: ${o}`
+        
     }
+    container.style.backgroundColor = "black"
 }
 let count = 0
 function claimBlock(e){
     i = e.target.id
-    if(gameobj[i] == ""){
+    if(gameobj[i] === "" && winner == false){
+        fingamediv.style.display = "block"
         o = getPlayer()
         gameobj[i] = o
+        if(o === "x"){
+            showwin.textContent = "player's turn: o"
+        }
+        if(o === "o"){
+            showwin.textContent = "player's turn: x"
+        }
+        e.target.innerHTML = o
         console.log(game[2])
     }
     if(!winner){
         getWinner()
     }
 }
-function changeUI(e){
-    e.target.innerHTML = o
-}
 
 function alertWinner(){
     if(winner){
         form.style.display = "none"
         fingamediv.style.display = "inline"
-        let showwin = document.getElementById("showwin")
-        showwin.innerHTML = winningplayer
+        newgamebut.style.display = "inline"
+        showwin.innerHTML = `${winningplayer} is the winner`
     }
     
 }
@@ -98,6 +115,7 @@ function getPlayer(){
 }
 
 function getWinner(){
+    //check for tie
     if(gameobj[0] !== "" && gameobj[1] !== "" && gameobj[2] !== "" && gameobj[3] !== "" && gameobj[4] !== "" && gameobj[5] !== "" && gameobj[6] !== "" && gameobj[7] !== "" && gameobj[8] !== ""){
         winningplayer = "it's a tie"
         winner = true
